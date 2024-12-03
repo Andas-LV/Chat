e9b45fd237a10b8d3816294bc3ab3d75ac6f4a75
Andas-LV <andassatybaldy@gmail.com>
2024-12-03 20:59:27 +0500

    node

--- client/src/socket/index.ts
+++ client/src/socket/index.ts
@@ -2,7 +2,7 @@ import { io } from 'socket.io-client';
 
 const SOCKET_URL = import.meta.env.VITE_BACKEND_URL;
 
-export const socket = io(`wss://${SOCKET_URL}`, {
+export const socket = io(`ws://${SOCKET_URL}`, {
     withCredentials: true, // Если нужно передавать куки
     auth: {
         token: localStorage.getItem('token'),
